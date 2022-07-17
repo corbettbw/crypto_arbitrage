8839f532b00583e61d6f0413ee6f2c423f9819ee
Corbett Winningham <Corbett Winningham>
2022-07-17 16:30:46 -0700

    adding env variables

--- helpful_methods.rb
+++ helpful_methods.rb
@@ -3,8 +3,8 @@ require 'coinbase/wallet'
 api_pass = 
 
 def setupClient
-    api_key =
-    api_secret = 
+    api_key = ENV['COINBASE_API_KEY']
+    api_secret = ENV['COINBASE_API_SECRET']
 
     Coinbase::Wallet::Client.new(api_key: api_key, api_secret: api_secret)
 end
